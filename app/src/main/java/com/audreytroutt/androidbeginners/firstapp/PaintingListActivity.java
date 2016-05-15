6739d61dd86b80f7cac460737c7adb04c68a2746
Audrey Troutt <audrey.troutt@gmail.com>
2016-05-15 16:31:32 -0400

    Fix the toolbar titles and back button for the list view.

--- app/src/main/java/com/audreytroutt/androidbeginners/firstapp/PaintingListActivity.java
+++ app/src/main/java/com/audreytroutt/androidbeginners/firstapp/PaintingListActivity.java
@@ -1,10 +1,7 @@
 package com.audreytroutt.androidbeginners.firstapp;
 
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
