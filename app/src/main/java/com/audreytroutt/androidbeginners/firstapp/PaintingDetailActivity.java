75665bdde9b898fc2722a1ef7dafe0252640443b
Audrey Troutt <audrey.troutt@gmail.com>
2016-05-15 17:24:12 -0400

    Layout the detail screen differently. This should be a scroll view, but that's a task for another day.

--- app/src/main/java/com/audreytroutt/androidbeginners/firstapp/PaintingDetailActivity.java
+++ app/src/main/java/com/audreytroutt/androidbeginners/firstapp/PaintingDetailActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,8 +25,6 @@ public class PaintingDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painting_detail);
 
-        getSupportActionBar().hide();
-
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             paintingId = extras.getInt("painting_id");
@@ -38,18 +37,17 @@ public class PaintingDetailActivity extends AppCompatActivity {
         int year = res.obtainTypedArray(R.array.painting_years).getInt(paintingId, 0);
         Drawable paintingDrawable = res.obtainTypedArray(R.array.paintings).getDrawable(paintingId);
 
-        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbar.setTitle(artist);
+        this.setTitle(artist);
 
         GradientDrawable gradientDrawable = new GradientDrawable();
         gradientDrawable.setShape(GradientDrawable.RECTANGLE);
         gradientDrawable.setColor(Color.WHITE);
         Picasso.with(this).load(res.obtainTypedArray(R.array.paintings).getResourceId(paintingId, 0)).placeholder(gradientDrawable).into((ImageView) findViewById(R.id.pd_image));
 
-        TextView descView = (TextView) findViewById(R.id.pd_description);
-        descView.setText(desc);
-
         TextView titleYearView = (TextView) findViewById(R.id.pd_title_year);
         titleYearView.setText(title + " (" + year + ")");
+
+        TextView descView = (TextView) findViewById(R.id.pd_description);
+        descView.setText(desc);
     }
 }
